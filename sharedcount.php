68d709384bb978ff2da2b942b4117790f89d51aa
Ben Doherty <ben@thinkoomph.com>
2015-07-29 17:04:41 -0400

    Plugin header

---
+++ sharedcount.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Plugin Name: Sharedcount
+ * Plugin URI: http://www.oomphinc.com/plugins/sharedcount
+ * Description: Pull, cache, and expose share counts from sharedcount.com.
+ * Author: Ben Doherty @ Oomph, Inc
+ * Author URI: http://www.oomphinc.com
+ * Version: 0.1
+ * Text Domain: sharedcount
+ * Domain Path: languages
+ * Credits: This integration is a distillation of integration found in the mashshare plugin. HT to René Hermenau.
+ *
+ * This SharedCount WordPress plugin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * This SharedCount WordPress plugin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Mashshare Share Buttons. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package SHAREDCOUNG
+ * @category Code
+ * @author Ben Doherty bendohmv@gmail.com
+ * @version 0.1
+ */
+
